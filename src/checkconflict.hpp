153419d69c03f2d99f6a2943280367067a60df92
NULLCT <cockatiel.u10@gmail.com>
2021-02-01 21:27:52 +0900

    fix wall conflict bug

---
+++ src/checkconflict.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+using namespace std;
+
+class CheckConflict {
+private:
+  vector<sf::RectangleShape> rects;
+  vector<bool> trs;
+
+public:
+
+  void setSize(int _size) {
+    rects.resize(_size);
+    trs.resize(_size);
+  }
+
+  CheckConflict(int _size) {
+    setSize(_size);
+  }
+
+  void update(sf::RenderWindow* _window, vector<sf::VertexArray>* rays, vector<sf::VertexArray>* walls, sf::Vector2f _charapos, int _raylength) {
+    for (auto& tr : trs)
+      tr = false;
+
+    for (auto& wall : *walls) {
+      int viewcnt = 1;
+      for (auto& ray : *rays) {
+        /*
+          ray[0].pos => ray[1].posw
+          wall[0].pos => wall[1].pos
+        */
+
+        double raytilt = (ray[0].position.y - ray[1].position.y) / (ray[0].position.x - ray[1].position.x);
+        double rayadd = ray[0].position.y - (ray[0].position.x * raytilt);
+        double walltilt = (wall[0].position.y - wall[1].position.y) / (wall[0].position.x - wall[1].position.x);
+        double walladd = wall[0].position.y - (wall[0].position.x * walltilt);
+
+        double crossxpos = (rayadd - walladd) / -(raytilt - walltilt);
+        double crossypos = (crossxpos * raytilt) + rayadd;
+
+        if (isinf(raytilt)) {
+          crossxpos = ray[0].position.x;
+          crossypos = (crossxpos * walltilt) + walladd;
+        }
+
+        //draw conflict pos
+        if ((wall[0].position.x <= crossxpos && crossxpos <= wall[1].position.x) || (wall[1].position.x <= crossxpos && crossxpos <= wall[0].position.x)) {
+          if ((ray[0].position.x <= crossxpos && crossxpos <= ray[1].position.x) || (ray[1].position.x <= crossxpos && crossxpos <= ray[0].position.x)) {
+            constexpr int size = 5;
+            sf::CircleShape circle(size);
+            circle.setOrigin(size, size);
+            circle.setPosition(crossxpos, crossypos);
+            _window->draw(circle);
+
+            double lengthfromchara = sqrt(pow(crossxpos - _charapos.x, 2) + pow(crossypos - _charapos.y, 2));
+
+            if (trs[viewcnt - 1]) {
+              if (rects[viewcnt - 1].getSize().y > (_raylength - lengthfromchara))
+                continue;
+            }
+            else {
+              trs[viewcnt - 1] = true;
+            }
+
+            sf::RectangleShape rect;
+            rects[viewcnt - 1].setSize(sf::Vector2f(1000 / rays->size(), (_raylength - lengthfromchara)));
+            rects[viewcnt - 1].setPosition(sf::Vector2f(2000 - (viewcnt * (1000 / rays->size())), 100 + (lengthfromchara) / 2));
+            rects[viewcnt - 1].setFillColor(sf::Color((1 - lengthfromchara / double(_raylength)) * 255, (1 - lengthfromchara / double(_raylength)) * 255, (1 - lengthfromchara / double(_raylength)) * 255));
+          }
+        }
+
+        viewcnt++;
+      }
+    }
+
+    for (int i = 0; i < rects.size(); i++) {
+      if(trs[i])
+        _window->draw(rects[i]);
+    }
+  }
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -4,53 +4,9 @@
 #include "chara.hpp"
 #include "rays.hpp"
 #include "walls.hpp"
+#include "checkconflict.hpp"
 
 using namespace std;
-
-void checkConflictAndDraw(sf::RenderWindow *_window, vector<sf::VertexArray>* rays, vector<sf::VertexArray>* walls,sf::Vector2f _charapos,int _raylength) {
-  for (auto& wall : *walls) {
-    int viewcnt = 1;
-    for (auto& ray : *rays) {
-      /*
-        ray[0].pos => ray[1].posw
-        wall[0].pos => wall[1].pos
-      */
-
-      double raytilt = (ray[0].position.y - ray[1].position.y) / (ray[0].position.x - ray[1].position.x);
-      double rayadd = ray[0].position.y - (ray[0].position.x * raytilt);
-      double walltilt = (wall[0].position.y - wall[1].position.y) / (wall[0].position.x - wall[1].position.x);
-      double walladd = wall[0].position.y - (wall[0].position.x * walltilt);
-
-      double crossxpos = (rayadd - walladd) / -(raytilt - walltilt);
-      double crossypos = (crossxpos * raytilt) + rayadd;
-
-      if (isinf(raytilt)) {
-        crossxpos = ray[0].position.x;
-        crossypos = (crossxpos * walltilt) + walladd;
-      }
-
-      //draw conflict pos
-      if ((wall[0].position.x <= crossxpos && crossxpos <= wall[1].position.x) || (wall[1].position.x <= crossxpos && crossxpos <= wall[0].position.x)) {
-        if ((ray[0].position.x <= crossxpos && crossxpos <= ray[1].position.x) || (ray[1].position.x <= crossxpos && crossxpos <= ray[0].position.x)) {
-          constexpr int size = 5;
-          sf::CircleShape circle(size);
-          circle.setOrigin(size, size);
-          circle.setPosition(crossxpos, crossypos);
-          _window->draw(circle);
-
-          double lengthfromchara = sqrt(pow(crossxpos - _charapos.x, 2) + pow(crossypos - _charapos.y, 2));
-          sf::RectangleShape rect;
-          rect.setSize(sf::Vector2f(1000 / rays->size(), (_raylength - lengthfromchara)));
-          rect.setPosition(sf::Vector2f(2000 - (viewcnt * (1000 / rays->size())), 100 + (lengthfromchara)/2));
-          rect.setFillColor(sf::Color((1 - lengthfromchara / double(_raylength)) * 255, (1- lengthfromchara / double(_raylength)) * 255, (1 -lengthfromchara / double(_raylength)) * 255));
-          _window->draw(rect);
-        }
-      }
-
-      viewcnt++;
-    }
-  }
-}
 
 int main() {
   sf::RenderWindow window(sf::VideoMode(2000, 1000), "Doomer");
@@ -58,8 +14,10 @@ int main() {
   window.setVerticalSyncEnabled(false);
 
   Chara chara;
-  Rays rays(64, 256);
+  Rays rays(64, 512);
   Walls walls;
+  CheckConflict checkconflicter(rays.getRays()->size());
+
   bool mousetr = false;
 
   while (window.isOpen()) {
@@ -76,7 +34,6 @@ int main() {
     if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
       if (!mousetr) {
         mousetr = true;
-        cout << "d\n";
         walls.getWalls()->push_back(sf::VertexArray(sf::LineStrip, 2));
         (walls.getWalls()->end()-1)[0][0].position = sf::Vector2f(sf::Mouse::getPosition(window).x,sf::Mouse::getPosition(window).y);
         (walls.getWalls()->end()-1)[0][1].position = chara.getPosition();
@@ -93,7 +50,7 @@ int main() {
     chara.update(&window);
     rays.update(&window, chara.getPosition());
     walls.update(&window);
-    checkConflictAndDraw(&window,rays.getRays(), walls.getWalls(),chara.getPosition(),rays.getRayLength());
+    checkconflicter.update(&window,rays.getRays(), walls.getWalls(),chara.getPosition(),rays.getRayLength());
 
     window.display();
   }
